e2e3dcc02beea2ba548f894a894422fd36a225ae
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-07-24 18:00:55 +0530

    add register controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,20 @@
 import  { Router } from "express"
 import { registerUser } from "../controllers/user.controller.js"
-
+import { upload } from '../middlewares/multer.middleware.js'
 const router = Router()
 
 // router.post("/register", registerUser)  // It's simple and is commonly used when a route has only one HTTP method.
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1,
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1,
+        },
+    ]),
+    registerUser)
 
 export default router
